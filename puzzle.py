f822b4f5acf0d5ace501f988919085007f5e2553
chiri001 <renniekipchirchir.com>
2023-12-05 23:19:55 -0500

    head-mvt

--- puzzle.py
+++ puzzle.py
@@ -1,5 +1,47 @@
 import pygame
 import random
+import cv2
+import pyautogui
+from multiprocessing import Process
+
+
+def find_and_move_to_face():
+    # Load the pre-trained face detection model
+    face_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_frontalface_default.xml')
+ 
+    # Start the video capture
+    video_capture = cv2.VideoCapture(0)
+ 
+    while True:
+        # Read a frame from the video capture
+        ret, frame = video_capture.read()
+ 
+        # Convert the frame to grayscale for face detection
+        gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+ 
+        # Detect faces in the frame
+        faces = face_cascade.detectMultiScale(gray, scaleFactor=1.1, minNeighbors=5, minSize=(30, 30))
+ 
+        # Iterate over the detected faces
+        for (x, y, w, h) in faces:
+            # Get the center position of the face
+            face_center_x = x + w // 2
+            face_center_y = y + h // 2
+ 
+            # Move the cursor to the center of the face
+            pyautogui.moveTo(face_center_x, face_center_y)
+ 
+        # Display the frame with face detection
+        #cv2.imshow('Face Detection', frame)
+ 
+        # Break the loop if 'q' is pressed
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            break
+ 
+    # Release the video capture and close the window
+    video_capture.release()
+    cv2.destroyAllWindows()
+
 
 def load_and_scale_image(image_path, max_width, max_height):
     image = pygame.image.load(image_path)
@@ -36,9 +78,10 @@ def main(image_path, num_pieces):
 
     # Load and scale the image
     image = load_and_scale_image(image_path, screen_width, screen_height)
-
     # Slice the image
     pieces = slice_image(image, num_pieces)
+
+    orig = pieces.copy()
 
     # Shuffle the pieces
     random.shuffle(pieces)
@@ -58,6 +101,13 @@ def main(image_path, num_pieces):
     while running:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
+                running = False
+            complete = True
+            for i in range (len(pieces)):
+                if pieces[i] != orig[i]:
+                    complete = False
+            if complete:
+                print("You did it")
                 running = False
             elif event.type == pygame.MOUSEBUTTONDOWN:
                 # Get current mouse position
@@ -82,10 +132,22 @@ def main(image_path, num_pieces):
                     pygame.display.flip()
                     indecies = []
 
-                    print(f"Swapped pieces at indices {indecies}")
-
 
     pygame.quit()
 
+
 if __name__ == "__main__":
-    main("dog.jpg", 4)  # Ensure the image path is correct
+    image_path = "dog.jpg"
+    num_slices = 5
+
+    cursor_process = Process(target=find_and_move_to_face)
+    image_process = Process(target=main, args=(image_path, num_slices))
+
+    #start process
+    
+    image_process.start()
+    cursor_process.start()
+
+    
+    image_process.join()
+    cursor_process.join()
